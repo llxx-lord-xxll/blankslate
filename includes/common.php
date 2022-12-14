3b5f72540349bc8b85782b54e7296d0b26a5f9a3
Arifuzzaman Pranto <arifuzzamanpranto@ymail.com>
2022-12-14 21:45:56 +0600

    Added reading time calculation functionalty

--- includes/common.php
+++ includes/common.php
@@ -186,3 +186,20 @@ function custom_search_form( $form ) {
     return $form;
 }
 add_filter( 'get_search_form', 'custom_search_form', 40 );
+
+function prefix_estimated_reading_time($post_id) {
+    // get the content
+    $the_content = get_post($post_id)->post_content;
+    // count the number of words
+    $words = str_word_count( strip_tags( $the_content ) );
+    // rounding off and deviding per 200 words per minute
+    $minute = floor( $words / 200 );
+    // rounding off to get the seconds
+    $second = floor( $words % 200 / ( 200 / 60 ) );
+    // calculate the amount of time needed to read
+    $estimate = $minute . ' minute' . ( $minute == 1 ? '' : 's' ) . ', ' . $second . ' second' . ( $second == 1 ? '' : 's' );
+    // create output
+    $output =  $estimate . ' read';
+    // return the estimate
+    return $output;
+}
